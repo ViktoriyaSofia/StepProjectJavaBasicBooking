669565d58a6029472a118efe7bbaf374b967559d
УкрТехноСервис <rvy@ukr.net>
2021-11-04 19:19:17 +0200

    save

--- src/com/booking/app/App.java
+++ src/com/booking/app/App.java
@@ -11,22 +11,7 @@ public class App {
 
         System.out.println("Welcome to the best flight booking app!");
 
-
-        Passenger p1 = new Passenger("Ivan", "Petrov");
-        Passenger p2 = new Passenger("Pineloppa", "Zdurovskaya");
-        Passenger p3 = new Passenger("Armageddon", "Fioletovich");
-
-        Booking b1 = new Booking(15, "LA",
-                LocalDate.of(2021, 11, 30), 2);
-        b1.getpL().add(p1);
-
-        Booking b2 = new Booking(17, "NY",
-                LocalDate.parse("20:09:2021", DateTimeFormatter.ofPattern("dd:MM:yyyy")), 3);
-
-        b1.getpL().add(p2);
-        b2.getpL().add(p3);
-
-        System.out.println(b1);
-        System.out.println(b2);
+        Booking.createDefaultBookings();
     }
+
 }
--- src/com/booking/app/domain/booking/Booking.java
+++ src/com/booking/app/domain/booking/Booking.java
@@ -8,6 +8,25 @@ import java.util.List;
 
 public class Booking implements Serializable {
     private static int bookingIDCounter;
+    public static void createDefaultBookings() {
+
+        Passenger p1 = new Passenger("Ivan", "Petrov");
+        Passenger p2 = new Passenger("Pineloppa", "Zdurovskaya");
+        Passenger p3 = new Passenger("Armageddon", "Fioletovich");
+
+        Booking b1 = new Booking(15, "LA",
+                LocalDate.of(2021, 11, 30), 2);
+        b1.getpL().add(p1);
+
+        Booking b2 = new Booking(17, "NY",
+                LocalDate.parse("20:09:2021", DateTimeFormatter.ofPattern("dd:MM:yyyy")), 3);
+
+        b1.getpL().add(p2);
+        b2.getpL().add(p3);
+
+        System.out.println(b1);
+        System.out.println(b2);
+    }
 
     private int flightID;
     private int bookingID;
