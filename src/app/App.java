27037ec31ad57d03f8eda7740e335241a520d141
УкрТехноСервис <rvy@ukr.net>
2021-11-08 03:09:28 +0200

    save

--- src/app/App.java
+++ src/app/App.java
@@ -4,10 +4,10 @@ import app.domain.booking.Booking;
 import app.domain.booking.Passenger;
 import app.domain.console.Console;
 import app.domain.console.wrongInputDataException.WrongInputDataException;
+import app.services.BookingService;
 
 import java.io.IOException;
 import java.util.List;
-
 public class App {
     public static void main(String[] args) throws IOException, WrongInputDataException, ClassNotFoundException {
         System.out.println("Welcome to the best flight booking app!");
