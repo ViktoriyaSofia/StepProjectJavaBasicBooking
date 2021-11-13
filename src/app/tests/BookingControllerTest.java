c0a9ce333ceb61e46ebb93c544610cd0ffbaa786
admin <1111>
2021-11-13 17:56:17 +0200

    resolve conflict

--- src/app/tests/BookingControllerTest.java
+++ src/app/tests/BookingControllerTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package app.tests;
 
 
@@ -19,8 +18,8 @@ public class BookingControllerTest {
     @Before
     public void setUp(){
         List<Passenger> p = new ArrayList<>(List.of(new Passenger("Vlad", "Ivanov")));
-         Booking b = new Booking(0, p);
-         b.setBookingTestID("testID_is_2021");
+        Booking b = new Booking(0, p);
+        b.setBookingTestID("testID_is_2021");
 
     }
 
@@ -32,38 +31,3 @@ public class BookingControllerTest {
     }
 
 }
-=======
-//package app.tests;
-//
-//
-//import app.domain.booking.Booking;
-//import app.domain.booking.Passenger;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.jupiter.api.BeforeAll;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.hamcrest.CoreMatchers.is;
-//import static org.junit.Assert.assertThat;
-//
-//
-//public class BookingControllerTest {
-//    @Before
-//    public void setUp(){
-//        List<Passenger> p = new ArrayList<>(List.of(new Passenger("Vlad", "Ivanov")));
-//         Booking b = new Booking(0, p);
-//         b.setBookingTestID("testID_is_2021");
-//
-//    }
-//
-//    @Test
-//    public void deleteBookingById_Successful(String TestID ){
-//
-//        boolean result = false;
-//        assertThat(result, is(false));
-//    }
-//
-//}
->>>>>>> dev
